cce7b529c1d76d1dc6fdb6238d8629cf11658678
Shinnosuke Takeda <shinnosuke@gmail.com>
2018-01-21 23:26:27 +0900

    rough impl.

---
+++ handler.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/cnosuke/jalindi/pb"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/lestrrat/go-fluent-client"
+	"go.uber.org/zap"
+)
+
+type handler struct {
+	jalindi.JalindiServiceServer
+	fl     *fluent.Buffered
+	tag    string
+	logger *zap.SugaredLogger
+}
+
+func NewHandler(fl *fluent.Buffered, tag string, logger *zap.SugaredLogger) *handler {
+	return &handler{
+		fl:     fl,
+		tag:    tag,
+		logger: logger,
+	}
+}
+
+func (h *handler) PostEvent(ctx context.Context, req *jalindi.PostEventRequest) (res *jalindi.PostEventResponse, err error) {
+	err = h.post(
+		req.Event,
+		req.Client,
+		req.BrowserUuid,
+		req.RequestUuid,
+		req.UserAgent,
+		req.Referer,
+	)
+
+	if err != nil {
+		h.logger.Errorw("Post failed",
+			"error", err,
+			"event", req.Event,
+			"client", req.Client,
+			"browserUuid", req.BrowserUuid,
+			"requestUuid", req.RequestUuid,
+			"userAgent", req.UserAgent,
+			"referer", req.Referer,
+		)
+
+		return nil, err
+	} else {
+		return &jalindi.PostEventResponse{}, nil
+	}
+}
+
+func (h *handler) PostEventList(ctx context.Context, req *jalindi.PostEventListRequest) (res *jalindi.PostEventListResponse, err error) {
+	for _, ev := range req.Events {
+		err = h.post(
+			ev,
+			req.Client,
+			req.BrowserUuid,
+			req.RequestUuid,
+			req.UserAgent,
+			req.Referer,
+		)
+
+		if err != nil {
+			h.logger.Errorw("PostList failed",
+				"error", err,
+				"event", ev,
+				"client", req.Client,
+				"browserUuid", req.BrowserUuid,
+				"requestUuid", req.RequestUuid,
+				"userAgent", req.UserAgent,
+				"referer", req.Referer,
+			)
+
+			return nil, err
+		}
+	}
+
+	return &jalindi.PostEventListResponse{}, nil
+}
+
+func (h *handler) post(event *jalindi.Event, client *jalindi.Client, browserUuid string, requestUuid string, userAgent string, referer string) (err error) {
+
+	m := map[string]interface{}{
+		"event": map[string]interface{}{
+			"type":   event.Type,
+			"group":  event.Group,
+			"action": event.Action,
+			"amount": event.Amount,
+		},
+		"client": map[string]interface{}{
+			"name":             client.Name,
+			"version":          client.Version,
+			"platform":         client.Platform,
+			"platform_version": client.PlatformVersion,
+			"device_name":      client.DeviceName,
+			"experiment":       client.Experiment,
+		},
+		"browser_uuid": browserUuid,
+		"request_uuid": requestUuid,
+		"user_agent":   userAgent,
+		"referer":      referer,
+	}
+
+	var t time.Time
+	if event.Timestamp != nil {
+		t, err = ptypes.Timestamp(event.Timestamp)
+		if err != nil {
+			return err
+		}
+	} else {
+		t = time.Now()
+	}
+
+	err = h.fl.Post(
+		h.tag,
+		m,
+		fluent.WithTimestamp(t.In(time.UTC)),
+		fluent.WithSyncAppend(true),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "github.com/k0kubun/pp"
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/cnosuke/jalindi/pb"
+	"github.com/lestrrat/go-fluent-client"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var logger *zap.SugaredLogger
 
 func main() {
-	pp.Println("hello")
+	zapConfig := zap.NewDevelopmentConfig()
+	zapConfig.OutputPaths = []string{"stdout"}
+	zapLogger, err := zapConfig.Build()
+	if err != nil {
+		fmt.Printf("Building logger error: %v", err)
+		os.Exit(1)
+	}
+
+	defer zapLogger.Sync()
+	logger = zapLogger.Sugar()
+
+	undo := zap.ReplaceGlobals(zapLogger)
+	defer undo()
+
+	binding := "localhost:8888"
+
+	listener, err := net.Listen("tcp", binding)
+
+	grpcServerOptions := []grpc.ServerOption{}
+
+	svr := grpc.NewServer(grpcServerOptions...)
+
+	fl, _ := fluent.NewBuffered()
+
+	handler := NewHandler(fl, "events", logger)
+
+	jalindi.RegisterJalindiServiceServer(
+		svr,
+		handler,
+	)
+
+	reflection.Register(svr)
+
+	if err := svr.Serve(listener); err != nil {
+		logger.Fatal(err)
+	}
+
 }
